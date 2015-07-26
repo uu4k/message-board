5d168caf1dafd9bfb0f3e8b82f254405ed117f42
uu4k <t.shimizu1115@gmail.com>
2015-07-26 09:22:47 +0000

    add edit feature

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
+  
   def index
     @messages = Message.all
     @message = Message.new
@@ -15,10 +17,25 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path, notice: 'メッセージを編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
   private
   def message_params
     # params[:message]のパラメータで name , bodyのみを許可する。
     # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
