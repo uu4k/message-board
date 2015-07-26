80a8bb8038b32370071087bdf3eb547856cab9e9
uu4k <t.shimizu1115@gmail.com>
2015-07-26 22:02:48 +0000

    add destroy feature

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
     @messages = Message.all
@@ -26,6 +26,11 @@ class MessagesController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   private
