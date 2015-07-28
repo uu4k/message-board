50ed9d1f750dfa480aa8365ba5174a1610a0e79f
uu4k <t.shimizu1115@gmail.com>
2015-07-28 22:15:26 +0000

    add MessageSpec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:message) { Message.new(params) }
+    let(:params) { {name: "テスト ユーザー", body:"テスト=ユーザーです.", age:20} }
+    describe "validates name" do
+        context "normal case" do
+            it "minimum length:1" do
+                message.name = "a"
+                expect(message.save).to eq true
+            end
+            it "maximum length:20" do
+                message.name = "a"*20
+                expect(message.save).to eq true
+            end
+        end
+        context "error case" do
+            it "minimum length over:0" do
+                message.name = ""
+                expect(message.save).to eq false
+            end
+            it "maximum length over:21" do
+                message.name = "a"*21
+                expect(message.save).to eq false
+            end
+        end
+    end
 end
