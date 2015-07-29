1d7dce4d9ca2db09a66a03447cac7ef8377eec6d
uu4k <t.shimizu1115@gmail.com>
2015-07-29 10:37:42 +0000

    edit MessageModelSpec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -25,4 +25,60 @@ describe Message do
             end
         end
     end
+    describe "validates body" do
+        context "normal case" do
+            it "minimum length:2" do
+                message.body = "a"*2
+                expect(message.save).to eq true
+            end
+            it "maximum length:30" do
+                message.body = "a"*30
+                expect(message.save).to eq true
+            end
+        end
+        context "error case" do
+            it "minimum length over:0" do
+                message.body = ""
+                expect(message.save).to eq false
+            end
+            it "minimum length over:1" do
+                message.body = "a"*1
+                expect(message.save).to eq false
+            end
+            it "maximum length over:31" do
+                message.body = "a"*31
+                expect(message.save).to eq false
+            end
+        end
+    end
+    describe "validates age" do
+        context "normal case" do
+            it "minimum length:0" do
+                message.age = 0
+                expect(message.save).to eq true
+            end
+            it "maximum length:200" do
+                message.age = 200
+                expect(message.save).to eq true
+            end
+        end
+        context "error case" do
+            it "minimum length over:-1" do
+                message.age = -1
+                expect(message.save).to eq false
+            end
+            it "minimum length over:201" do
+                message.age = 201
+                expect(message.save).to eq false
+            end
+            it "type error:string" do
+                message.age = "a"
+                expect(message.save).to eq false
+            end
+            it "type error:float" do
+                message.age = 1.1
+                expect(message.save).to eq true
+            end
+        end
+    end
 end
